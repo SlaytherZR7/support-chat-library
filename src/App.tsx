04a784342385017a98ef7b157ad2fc8e191111b6
Slayther Zamora <slayther.zr@gmail.com>
2025-11-04 09:12:52 -0500

    feat: implement chat layout and page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { AuthLayout } from "./auth/layout/AuthLayout";
+import ChatLayout from "./chat/layout/ChatLayout";
+import ChatPage from "./chat/pages/ChatPage";
 
 function App() {
   return (
     <>
-      <AuthLayout />
+      <ChatLayout>
+        <ChatPage />
+      </ChatLayout>
     </>
   );
 }
